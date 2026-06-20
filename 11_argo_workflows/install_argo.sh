a2678f45982b5cd8f63b9995445c0a432f6aed17
Ajay Singh Chauhan <107171838+AjaySinghChauhan85@users.noreply.github.com>
2026-06-20 18:48:11 +0530

    Update install_argo.sh

--- 11_argo_workflows/install_argo.sh
+++ 11_argo_workflows/install_argo.sh
@@ -4,17 +4,23 @@ if [[ "$(uname -s)" != "Darwin" ]]; then
   ARGO_OS="linux"
 fi
 
+# Detect architecture
+ARGO_ARCH="amd64"
+if [[ "$(uname -m)" == "arm64" || "$(uname -m)" == "aarch64" ]]; then
+  ARGO_ARCH="arm64"
+fi
+
 # Download the binary
-curl -sLO "https://github.com/argoproj/argo-workflows/releases/download/v3.7.0/argo-$ARGO_OS-amd64.gz"
+curl -sLO "https://github.com/argoproj/argo-workflows/releases/download/v4.0.6/argo-$ARGO_OS-$ARGO_ARCH.gz"
 
 # Unzip
-gunzip "argo-$ARGO_OS-amd64.gz"
+gunzip "argo-$ARGO_OS-$ARGO_ARCH.gz"
 
 # Make binary executable
-chmod +x "argo-$ARGO_OS-amd64"
+chmod +x "argo-$ARGO_OS-$ARGO_ARCH"
 
 # Move binary to path
-sudo mv "./argo-$ARGO_OS-amd64" /usr/local/bin/argo
+sudo mv "./argo-$ARGO_OS-$ARGO_ARCH" /usr/local/bin/argo
 
 # Test installation
 argo version
